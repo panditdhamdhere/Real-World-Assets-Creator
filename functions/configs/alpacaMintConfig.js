9433c199db00b7fbeafc91fbef15ce476953a8b0
panditdhamdhere <dhamdherepandit@gmail.com>
2024-07-20 12:24:22 +0530

    alpaca script

---
+++ functions/configs/alpacaMintConfig.js
@@ -0,0 +1,17 @@
+const fs = request('fs')
+const { Location, ReturnType, CodeLanguage } = require("@chainlink/functions-toolkit")
+
+
+const requestConfig = {
+    source: fs.readFileSync("./functions/sources/alpacaBalance.js"),
+    codeLocation: Location.Inline,
+    secrets: {
+        alpacaKey: process.env.ALPACA_API_KEY, alpacaSecrets: process.env.ALPACA_SECRET_KEY
+    },
+    secretsLocation: Location.DONHosted,
+    args: [],
+    CodeLanguage: CodeLanguage.JavaScript,
+    expectedReturnType: ReturnType.uint256,
+}
+
+module.exports = requestConfig
---
+++ functions/simulators/alpacaMintSimulator.js
@@ -0,0 +1,20 @@
+// TODO
+const requestConfig = require("../configs/alpacaMintConfig")
+const { simulateScript, decodeResult } = require("@chainlink/functions-toolkit")
+
+async function main() {
+    const { responseBytesHexstring, errorString, capturedTerminalOutput } = await simulateScript(requestConfig);
+
+    if (responseBytesHexstring) {
+        console.log(`Response returned by scipt: ${decodeResult(responseBytesHexstring, requestConfig.expectedReturnType).toString()}\n`)
+    }
+
+    if (errorString) {
+        console.log(`Error returned by script: ${errorString}\n`)
+    }
+}
+
+main().catch((error) => {
+    console.error(error)
+    process.exitCode = 1
+})
